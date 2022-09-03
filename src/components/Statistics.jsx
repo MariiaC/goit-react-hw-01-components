b0a387095b93c24d4f1edc2211629dc3b18eadc6
MariiaC <mariiachvorun@gmail.com>
2022-09-03 23:43:08 +0300

    upd

---
+++ src/components/Statistics.jsx
@@ -0,0 +1,27 @@
+// const Statistics=({title, stats }) => {
+//     return (
+//     <section class="statistics">
+//             <h2 class="title">{ title}</h2>
+
+//   <ul class="stat-list">
+//     <li class="item">
+//       <span class="label">{stats.label}</span>
+//                     <span class="percentage">{ stats.percentage}</span>
+//     </li>
+//     <li class="item">
+//       <span class="label">{stats.label}</span>
+//       <span class="percentage">{ stats.percentage}</span>
+//     </li>
+//     <li class="item">
+//       <span class="label">{stats.label}</span>
+//       <span class="percentage">{ stats.percentage}</span>
+//     </li>
+//     <li class="item">
+//       <span class="label">{stats.label}</span>
+//       <span class="percentage">{ stats.percentage}</span>
+//     </li>
+//   </ul>
+// </section>);
+// };
+
+// export default Statistics;
